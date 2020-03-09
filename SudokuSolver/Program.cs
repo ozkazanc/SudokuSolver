676d89b1725bfb2961714ce656c7265d9b54be19
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2020-03-09 16:09:54 -0400

    Reading the sudoku puzzle from solve_this.txt done

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,24 +12,26 @@ namespace SudokuSolver
         static int[,] board = new int[9, 9];
         static int totalSolutions = 0;
 
+        static readonly string fileName = "solve_this.txt";
+        static string path = Path.Combine(Environment.CurrentDirectory, @"..\..\", fileName);
+
         static void Main(string[] args)
         {
             Console.Write("Sudoku Solver\n\n");
 
-            InitBoard();
-            if (!IsValidBoard()) {
-                Console.WriteLine("The given board is not valid.");
-                Console.ReadLine();
+            if (!InitBoard())
                 return;
-            }
 
+            if (!IsValidBoard())              
+                return;
+            
             PrintBoard();
             Solve();
 
             if (totalSolutions == 0)
                 Console.WriteLine("\nNo possible solution were found.");
-
         }
+
         static void Solve() {
             for (int x = 0; x < 9; x++) {
                 for (int y = 0; y < 9; y++) {
@@ -77,80 +80,60 @@ namespace SudokuSolver
             return true;
         }
 
-        static void InitBoard() {
-            //board[0, 4] = 2;
-            //board[0, 5] = 1;
-            //board[0, 8] = 6;
-            //board[1, 1] = 7;
-            //board[1, 3] = 6;
-            //board[2, 2] = 5;
-            //board[2, 6] = 1;
-            //board[3, 0] = 9;
-            //board[3, 4] = 3;
-            //board[3, 7] = 2;
-            //board[4, 1] = 4;
-            //board[4, 2] = 2;
-            //board[4, 4] = 5;
-            //board[4, 6] = 3;
-            //board[4, 7] = 8;
-            //board[5, 1] = 6;
-            //board[5, 4] = 1;
-            //board[5, 8] = 5;
-            //board[6, 2] = 4;
-            //board[6, 6] = 2;
-            //board[7, 5] = 2;
-            //board[7, 7] = 3;
-            //board[8, 0] = 2;
-            //board[8, 3] = 8;
-            //board[8, 4] = 9;
+        static bool InitBoard() {
 
-            board[0, 0] = 4;
-            board[0, 1] = 8;
-            board[0, 4] = 5;
-            board[0, 6] = 1;
-            board[0, 7] = 3;
-            board[1, 4] = 1;
-            board[1, 5] = 6;
-            board[1, 8] = 5;
-            board[2, 1] = 5;
-            board[2, 5] = 8;
-            board[2, 6] = 9;
-            board[2, 7] = 2;
-            board[3, 0] = 3;
-            board[3, 1] = 1;
-            board[3, 3] = 8;
-            board[4, 2] = 9;
-            board[4, 3] = 1;
-            board[4, 5] = 7;
-            board[4, 6] = 8;
-            board[5, 5] = 9;
-            board[5, 7] = 1;
-            board[5, 8] = 2;
-            board[6, 1] = 3;
-            board[6, 2] = 1;
-            board[6, 3] = 2;
-            board[6, 7] = 4;
-            board[7, 0] = 9;
-            board[7, 3] = 7;
-            board[7, 4] = 8;
-            board[8, 1] = 2;
-            board[8, 2] = 5;
-            board[8, 4] = 4;
-            board[8, 7] = 7;
-            board[8, 8] = 9;
+            if (!File.Exists(path)) {
+                Console.WriteLine("Error: The file does not exist\n");
+                return false;
+            }
+
+            // Read entire text file content in one string    
+            string text = File.ReadAllText(path);
+
+            char[] delimiterChars = { ' ', ',', '\r', '\n', '\t' };
+            string[] words = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+
+            // We need at least 81 number to create a sudoku puzzle
+            if (words.Length < 81) {
+                Console.WriteLine("Error: The file does not contain a valid sudoku puzzle.\n");
+                return false;
+            }
+
+            for (int x = 0; x < 9; x++) {
+                for (int y = 0; y < 9; y++) {
+                    int number = 0;
+
+                    bool isParsable = Int32.TryParse(words[x * 9 + y], out number);
+
+                    if (isParsable)
+                        board[x, y] = number;
+
+                    else {
+                        Console.WriteLine("Error: The file does not contain a valid sudoku puzzle.\n");
+                        return false;
+                    }
+                }
+            }
+
+            return true;  
         }
 
         static bool IsValidBoard() {
             for (int x = 0; x < 9; x++) {
                 for (int y = 0; y < 9; y++) {
-                    if (board[x, y] != 0)  {
+                    if (board[x, y] != 0)
+                    {
+                        // Store the current board number at that position and 
+                        // try to insert it at the same position to see if it is valid
                         int temp = board[x, y];
                         board[x, y] = -1;
+
                         if(!IsPossible(x, y, temp)){
+                            Console.WriteLine("The given board is not valid.\n");
                             return false;
                         }
 
-                        board[x, y] = temp;
+                        board[x, y] = temp;     // restore the given number at the position
                     }
                 }
             }
